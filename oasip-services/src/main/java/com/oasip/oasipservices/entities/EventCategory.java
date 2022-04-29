04aba5607e23d2c5509376afe9136083a6a8504c
siraphop p <68784379+phop-parker@users.noreply.github.com>
2022-04-29 18:55:34 +0700

    create new backend dir

---
+++ oasip-services/src/main/java/com/oasip/oasipservices/entities/EventCategory.java
@@ -0,0 +1,55 @@
+package com.oasip.oasipservices.entities;
+
+import javax.persistence.*;
+
+@Table(name = "eventCategory", indexes = {
+        @Index(name = "eventCategoryName", columnList = "eventCategoryName", unique = true)
+})
+@Entity
+public class EventCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "categoryId", nullable = false)
+    private Integer id;
+
+    @Column(name = "eventCategoryName", nullable = false, length = 100)
+    private String eventCategoryName;
+
+    @Column(name = "eventCatagoryDescription", nullable = false, length = 400)
+    private String eventCatagoryDescription;
+
+    @Column(name = "eventDuration", nullable = false)
+    private Integer eventDuration;
+
+    public Integer getEventDuration() {
+        return eventDuration;
+    }
+
+    public void setEventDuration(Integer eventDuration) {
+        this.eventDuration = eventDuration;
+    }
+
+    public String getEventCatagoryDescription() {
+        return eventCatagoryDescription;
+    }
+
+    public void setEventCatagoryDescription(String eventCatagoryDescription) {
+        this.eventCatagoryDescription = eventCatagoryDescription;
+    }
+
+    public String getEventCategoryName() {
+        return eventCategoryName;
+    }
+
+    public void setEventCategoryName(String eventCategoryName) {
+        this.eventCategoryName = eventCategoryName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ oasip-services/src/test/java/com/oasip/oasipservices/OasipServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasip.oasipservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasipServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
