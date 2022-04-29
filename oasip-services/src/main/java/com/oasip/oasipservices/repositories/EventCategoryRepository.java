90f3ea64ac403dcae383d22b6c5c842673d5e06d
siraphop p <68784379+phop-parker@users.noreply.github.com>
2022-04-29 18:49:04 +0700

    DELETE SERVICES

--- oasip-services/src/main/java/com/oasip/oasipservices/repositories/EventCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oasip.oasipservices.repositories;
-
-import com.oasip.oasipservices.entities.Eventcategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventCategoryRepository extends JpaRepository<Eventcategory, Integer> {
-}
