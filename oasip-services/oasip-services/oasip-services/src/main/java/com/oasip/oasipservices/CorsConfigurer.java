2004b16790b292af1b60dab3f7a8964a93428eae
Tongtin1234 <73164501+Tongtin1234@users.noreply.github.com>
2022-05-01 17:19:25 +0700

    edit backend

---
+++ oasip-services/oasip-services/oasip-services/src/main/java/com/oasip/oasipservices/CorsConfigurer.java
@@ -0,0 +1,17 @@
+package com.oasip.oasipservices;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "HEAD", "POST", "DELETE");
+    }
+}
