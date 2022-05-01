e71a6d7f9d3bc58606f8847b3ac1ce876cacfb62
Tongtin1234 <piyamon.test@mail.kmutt.ac.th>
2022-05-01 21:32:06 +0700

    commit pbi1

--- OAIP-OR-2-project/src/views/ViewsAllAppointment.vue
+++ OAIP-OR-2-project/src/views/ViewsAllAppointment.vue
@@ -8,18 +8,20 @@ let events = ref([])
 
 // GET
 const getEvents = async () => {
-    const res = await fetch('/api/events')
+    const res = await fetch('http://10.4.56.95:8080/api/events')
     if (res.status === 200) {
-        console.log(res);
+        console.log("response reply")       
+        console.log(res);;
+	console.log(events.value);
         events.value = await res.json()
     } else
         console.log("error, cann't get data");
 }
+
 onBeforeMount(async () => {
     await getEvents()
     console.log(events)
 })
-
 </script>
  
 <template>
