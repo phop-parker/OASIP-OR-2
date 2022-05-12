13dd196510f494e4eb31d19f76f026cb8f3e4306
phxsrp <phxsrp@gmail.com>
2022-05-12 23:51:43 +0700

    UPDATE FORMAT DATE

--- oasip-services/src/main/java/com/oasip/oasipservices/DTOS/EventDTO.java
+++ oasip-services/src/main/java/com/oasip/oasipservices/DTOS/EventDTO.java
@@ -1,5 +1,6 @@
 package com.oasip.oasipservices.DTOS;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,13 +25,9 @@ public class EventDTO {
     private Integer id;
     private String bookingName;
     private String bookingEmail;
+    @JsonFormat(pattern="dd/MM/yyyy, HH:mm:ss")
     private LocalDateTime eventStartTime;
     private Integer eventDuration;
     private String eventNotes;
     private String eventCategoryName;
-
-    public String getEventStartTime(){
-        DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        return formatter.format(eventStartTime);
-    }
 }
