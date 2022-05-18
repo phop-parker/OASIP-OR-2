910980911d2c1030176d1adbfad84a310f925b2c
siraphop p <68784379+phop-parker@users.noreply.github.com>
2022-05-18 20:54:20 +0700

    Edit DTOS

--- oasip-services/src/main/java/com/oasip/oasipservices/DTOS/EventCategoryDTO.java
+++ oasip-services/src/main/java/com/oasip/oasipservices/DTOS/EventCategoryDTO.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class EventCategoriesDTO {
+public class EventCategoryDTO {
     private Integer id;
     private String eventCategoryName;
     private String eventCategoryDescription;
